a5608a12aec32ae492379348bcfb555f9e386510
Rex <rex@rex.gs>
2022-08-03 02:59:33 +0900

    installer

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,30 @@
 #!/bin/sh
+cd `dirname $0`
 
-ln -sr ./zshrc $HOME/.zshrc
-ln -sr ./config/nvim $HOME/.config/nvim
+if [[ ! -e $HOME/.zshrc ]]; then
+  ln -sr ./zshrc $HOME/.zshrc
+  echo "[+] Created symlink of .zshrc"
+fi
+
+if [[ ! -d $HOME/.config/nvim ]]; then
+  ln -sr ./config/nvim $HOME/.config/nvim
+  echo "[+] Created symlink of .config/nvim"
+fi
+
+if [[ ! -d $HOME/.tmux.conf ]]; then
+  ln -sr ./tmux.conf $HOME/.tmux.conf
+  echo "[+] Created symlink of .tmux.conf"
+fi
+
+# for WSL
+if [[ `uname -r` =~ .*WSL.* ]]; then
+  if [[ -z WINHOME ]];
+    export WINHOME=/mnt/c/Users/rex
+  fi
+
+  cp "$WINHOME/Application Data/wsltty/config" "$WINHOME/Application Data/wsltty/config.bak"
+  cp wsl/wsltty/config "$WINHOME/Application Data/wsltty/config"
+
+  cp "$WINHOME/Application Data/wsltty/config" "$WINHOME/Application Data/wsltty/config.bak"
+  cp wsl/wsltty/themes/favorite "$WINHOME/Application Data/wsltty/config"
+fi
---
+++ install.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+PYTHON_VERSION=3.10.2
+
+PACKAGES=(
+  # required
+  socat
+  git
+
+  # optional
+  build-essential
+  gdb-multiarch
+  tmux
+  sagemath
+  qemu-system-arm
+  qemu-user
+)
+
+sudo apt-get update
+
+echo "[+] Install packages"
+sudo apt install $PACKAGES
+
+if [[ `uname -r` =~ .*WSL.* ]]; then
+  echo "[+] Install wsl2-ssh-pageant"
+  WSL2_SSH_PAGEANT=$HOME/.ssh/wsl2-ssh-pageant.exe
+
+  sudo apt-get install socat git
+
+  if [[ ! -e $WSL2_SSH_PAGEANT ]]; then
+    wget -O $WSL2_SSH_PAGEANT https://github.com/BlackReloaded/wsl2-ssh-pageant/releases/download/v1.4.0/wsl2-ssh-pageant.exe
+  fi
+
+  setsid nohup socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"$WSL2_SSH_PAGEANT -logfile /dev/null" >/dev/null 2>&1 &
+
+fi
+
+
+echo "[+] Install Docker"
+sudo apt-get install  ca-certificates curl gnupg lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+
+echo "[+] Install NeoVim"
+wget -O $HOME/nvim https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x $HOME/nvim
+
+
+echo "[+] Install pyenv"
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+pyenv install $PYTHON_VERSION
+
