b78e793b2ad752cedb55ab36db3ea3a252d2d8f2
Rex <rex@rex.gs>
2024-06-06 10:55:29 +0900

    add wsl2-ssh-agent

--- config/zshconf/gadgets/wsl.zsh
+++ config/zshconf/gadgets/wsl.zsh
@@ -3,5 +3,6 @@ if [[ `uname -r` =~ .*WSL.* ]]; then
   export WINUSER=`whoami.exe | sed -r 's/^[^\]+\\\\(.+)\r$/\1/g'`
   export WINHOME=/mnt/c/Users/$WINUSER
   export SSH_SK_HELPER="/mnt/c/Program Files/OpenSSH/ssh-sk-helper.exe"
+  eval $(wsl2-ssh-agent)
 fi
 
